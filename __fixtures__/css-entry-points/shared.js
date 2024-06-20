03a00a6099ce25b449e91e9241be82945c14ba0c
Ferdinand Thiessen <opensource@fthiessen.de>
2024-06-20 11:45:25 +0200

    feat: Add `CSSEntryPointsPlugin` to fix vite for creating one CSS entry per JS entry point
    with `cssCodeSplit` vite only inlines CSS in async chunks,
    but it does not properly create CSS files for synchronously imported CSS.
    
    Co-authored-by: Ferdinand Thiessen <opensource@fthiessen.de>
    Co-authored-by: Grigorii K. Shartsev <me@shgk.me>
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ __fixtures__/css-entry-points/shared.js
@@ -0,0 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ferdinand Thiessen <opensource@fthiessen.de>
+ * SPDX-License-Identifier: CC0-1.0
+ */
+import './shared.css'
+
+window.something = () => 'Just so the module will not be empty'
