9e07b8e42387fb45ac9df00ca7af900e4afffed8
Ferdinand Thiessen <opensource@fthiessen.de>
2024-07-12 01:19:57 +0200

    feat(EmptyJSDirPlugin): Allow to specify other directories to clear like `css/`
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/plugins/EmptyJSDir.ts
+++ lib/plugins/EmptyJSDir.ts
@@ -9,18 +9,34 @@ import type { Plugin } from 'vite'
 import { existsSync, rmSync } from 'node:fs'
 import * as path from 'node:path'
 
+export interface EmptyJSDirPluginOptions {
+	/**
+	 * Additional directories to clear (e.g. 'css')
+	 * @default []
+	 */
+	additionalDirectories?: string[]
+}
+
 /**
  * Vite plugin to clear the `js/` directory before emitting files
+ *
+ * @param options Options to pass to the plugin
  */
-const EmptyJSDirPlugin = () => {
+function EmptyJSDirPlugin(options?: EmptyJSDirPluginOptions) {
 	let emptyJSDir = false
 
 	return {
 		name: 'nextcloud-empty-js',
 		generateBundle() {
 			if (emptyJSDir !== true) {
-				if (existsSync(path.resolve('js/'))) {
-					rmSync(path.resolve('js/'), { recursive: true })
+				const dirs = [
+					...(options?.additionalDirectories ?? []),
+					'js',
+				]
+				for (const dir of dirs) {
+					if (existsSync(path.resolve(dir))) {
+						rmSync(path.resolve(dir), { recursive: true })
+					}
 				}
 				emptyJSDir = true
 			}
