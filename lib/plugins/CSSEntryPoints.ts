e061548de1bc3d2f0af27adfc5366f18e9e06157
Ferdinand Thiessen <opensource@fthiessen.de>
2026-04-07 18:29:58 +0200

    chore: remove outdated eslint comments
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/plugins/CSSEntryPoints.ts
+++ lib/plugins/CSSEntryPoints.ts
@@ -1,9 +1,9 @@
-/**
+/*!
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
  * SPDX-FileCopyrightText: 2024 Ferdinand Thiessen <opensource@fthiessen.de>
  * SPDX-License-Identifier: MIT
  */
 
-// eslint-disable-next-line n/no-extraneous-import
 import type { OutputOptions, PreRenderedAsset } from 'rollup'
 import type { Plugin } from 'vite'
 
